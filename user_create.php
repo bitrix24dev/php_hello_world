8d5a84f7b5031b25c2dea7e991677b0dc895ce8f
Dmitri07 <dyuosetskiy@rt-dc.ru>
2023-02-21 11:42:49 +0300

    Merge remote-tracking branch 'origin/master'

--- user_create.php
+++ user_create.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'pdo.php';
+
 use Google\Cloud\Samples\CloudSQL\Postgres;
 
 // https://phpdelusions.net/pdo_examples/insert
@@ -8,16 +9,17 @@ $connection = Postgres\DatabaseUnix::initUnixDatabaseConnection();
 
 $time = time();
 
-$user = [
-  "email" => "123@mail.ru",
-  "created" => $time,
-  "updated" => $time,
-  "roles" => "",
-  "account_id" => 1,
+$data = [
+    "name" => "test",
+    "created" => $time,
+    "updated" => $time,
+    "email" => "123@mail.ru",
+    "account_id" => 1,
+    "roles" => "",
 ];
 
-$sql = "INSERT INTO users (email, created, updated,roles,account_id) VALUES (:name, :created, :updated, :roles, :account_id)";
-$connection->prepare($sql)->execute($user);
+$sql = "INSERT INTO users (name, created, updated, email, account_id, roles) VALUES (:name, :created, :updated, :email, :account_id,:roles)";
+$connection->prepare($sql)->execute($data);
 
 
 echo "User Created";
